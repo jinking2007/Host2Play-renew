d7045d8e7be30128c41ff7f8bd814e8c327b7b9c
KKKLEIN <31945072+jinking2007@users.noreply.github.com>
2026-08-28 21:44:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 
 # 配置区域
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=642fbe26-f94e-4b1a-b141-6016bbe3799d",
     # 添加更多链接
 ]
 
